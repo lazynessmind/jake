272da11135b9c4ee02f8aa2b61056c7b0fbe98bc
Marco Santos <lazynessmind@gmail.com>
2022-03-26 09:37:09 +0000

    Include support for external .jar files.

--- example/src/lazy/test/Main.java
+++ example/src/lazy/test/Main.java
@@ -1,8 +1,11 @@
 package lazy.test;
 
+import lazy.math.MathLib;
+
 public class Main {
 
     public static void main(String[] args){
         Hmm.print();
+        System.out.println(MathLib.sum(34, 35));
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ void build_project(int argc, char **argv)
     const char *arg = shift(&argc, &argv);
     auto props = parse_properties_file(jakefile.append(arg).append("/jakefile.properties").c_str());
 
+    std::string pwd = std::filesystem::current_path().string();
     const auto build_path = get_prop_or_return(props, "build_path", "./build");
 
     std::string sources;
@@ -27,14 +28,39 @@ void build_project(int argc, char **argv)
             if (p.path().filename().extension() == ".java")
                 sources.append(p.path()).append(" ");
 
+    //TODO: Change this after the move to json.
+    std::string libs;
+    if (has_prop(props, "libs_path"))
+    {
+        for (const auto &p : std::filesystem::recursive_directory_iterator(props["libs_path"]))
+            if (!std::filesystem::is_directory(p))
+                if (p.path().filename().extension() == ".jar")
+                {
+                    libs.append(".:");
+                    libs.append(p.path().string()).append(":");
+                    std::string extract_jar = "jar xf ";
+                    extract_jar.append(pwd).append("/").append(p.path().string());
+                    printf("$ %s\n", extract_jar.c_str());
+                    printf("$ cd %s\n", build_path.c_str());
+                    std::filesystem::current_path(build_path);
+                    exec(extract_jar.c_str());
+                    std::filesystem::current_path(pwd);
+                }
+    }
+
     // Compile the collected sources with the -d option to specify the build folder.
     if (!sources.empty())
     {
         printf("> Compile sources:\n");
-        std::string compile_command = "javac -d ";
-        compile_command.append(build_path)
-            .append(" ")
-            .append(sources);
+        std::string compile_command = "javac ";
+        if (!libs.empty())
+        {
+            compile_command
+                .append("-cp ")
+                .append(libs)
+                .append(" ");
+        }
+        compile_command.append("-d ").append(build_path).append(" ").append(sources);
         printf("$ %s\n", compile_command.c_str());
         exec(compile_command.c_str());
         printf("> Done!\n");
@@ -48,20 +74,21 @@ void build_project(int argc, char **argv)
 
     // Create the .jar file with the classes available in the `build_path` props and includes the `entry_point`.
     printf("> Create .jar file:\n");
-    std::string pwd = std::filesystem::current_path().string();
     printf("$ cd %s\n", build_path.c_str());
     std::filesystem::current_path(build_path);
     std::string jar_command = "jar ";
     if (has_prop(props, "entry_point"))
     {
         jar_command.append("cfe ");
+        jar_command.append(props["jar_name"]).append(".jar ");
         jar_command.append(props["entry_point"]).append(" ");
     }
     else
     {
         jar_command.append("cf ");
+        jar_command.append(props["jar_name"]).append(".jar ");
     }
-    jar_command.append(props["jar_name"]).append(".jar ").append("*");
+    jar_command.append("*");
     printf("$ %s\n", jar_command.c_str());
     exec(jar_command.c_str());
     printf("$ cd %s\n", pwd.c_str());
