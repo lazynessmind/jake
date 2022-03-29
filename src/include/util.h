24024f4331d7beb3057717c69527adfba29a566d
Marco Santos <lazynessmind@gmail.com>
2022-03-29 20:34:32 +0100

    Removed dead code.

--- src/include/util.h
+++ src/include/util.h
@@ -8,11 +8,7 @@
 #include "./json.hpp"
 
 std::string exec(const char *);
-std::vector<std::string> split_by_delimiter(std::string, std::string);
 char *shift(int *, char ***);
-std::map<std::string, std::string> parse_properties_file(const char *);
-std::string get_prop_or_return(std::map<std::string, std::string>, std::string, std::string);
-bool has_prop(std::map<std::string, std::string>, std::string);
 nlohmann::json load_json(const char *);
 std::string try_get(const nlohmann::json &, const char *);
 std::string try_get_or(const nlohmann::json &, const char *, const char *);
